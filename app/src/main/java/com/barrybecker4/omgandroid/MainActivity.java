ce6828b752863d6e6eef4420fb5edf85b5c9f04a
Barry G. Becker <barrybecker4@yahoo.com>
2015-09-12 08:02:26 -0700

    OMGAndroid tutorial - working through second part of tutorial.

--- app/src/main/java/com/barrybecker4/omgandroid/MainActivity.java
+++ app/src/main/java/com/barrybecker4/omgandroid/MainActivity.java
@@ -3,14 +3,35 @@ package com.barrybecker4.omgandroid;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    private TextView mainTextView;
+    private Button mainButton;
+    private int numClicks = 0;
+    private EditText mainEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 1. Access the TextView defined in layout XML
+        // and then set its text
+        mainTextView = (TextView) findViewById(R.id.main_textview);
+        mainTextView.setText("Set in Java!");
+
+        // 2. Access the Button defined in layout XML and listen for it here
+        mainButton = (Button) findViewById(R.id.main_button);
+        mainButton.setOnClickListener(this);
+
+        // 3. Access the EditText defined in layout XML
+        mainEditText = (EditText) findViewById(R.id.main_edittext);
+
     }
 
     @Override
@@ -20,18 +41,17 @@ public class MainActivity extends Activity {
         return true;
     }
 
+    /** Called when main_button clicked */
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    public void onClick(View v) {
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+        numClicks++;
+        // change the text when the button is clicked.
+        mainTextView.setText("Button pressed " + numClicks + " times.");
 
-        return super.onOptionsItemSelected(item);
+        // Take what was typed into the EditText and use in TextView
+        mainTextView.setText(mainEditText.getText().toString()
+                + " is learning Android development! (" + numClicks + ")");
+
     }
 }
